bc8b3f0472d6345acb8b6b2681b1c1975779397f
MichalRais <Michal.Rais@iCloud.com>
2025-09-10 16:56:20 +0100

    App.jsx: add label and input for search

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 
+const title = 'React'
+
 function App() {
 
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Hello {title}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />            
     </div>
   )
 }
