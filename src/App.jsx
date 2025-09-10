4962ef74bbd0e0c6993625e557e619f8566209ea
MichalRais <Michal.Rais@iCloud.com>
2025-09-10 17:36:15 +0100

    App.jsx: create array of objects list

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import './App.css'
 
-function getTitle(title) {
-  return title
-}
-
-const numbers = [1,2,3,4]
-
-const exponentialNumbers = numbers.map(function (number) {
-  return number * number
-})
-
-console.log(exponentialNumbers);
-
-
+const list = [
+  {
+    title: 'React',
+    url: 'http://react.dev',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'http://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
 function App() {
 
   return (
     <div>
-      <h1>{getTitle('React')}</h1>
+      <h1>React</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />            
     </div>
