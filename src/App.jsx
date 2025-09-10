c2db0e674517ee334b2f667b0cb6f6f330a9eb4c
MichalRais <Michal.Rais@iCloud.com>
2025-09-10 17:29:55 +0100

    App.jsx: create array numbers and map it

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ function getTitle(title) {
   return title
 }
 
+const numbers = [1,2,3,4]
+
+const exponentialNumbers = numbers.map(function (number) {
+  return number * number
+})
+
+console.log(exponentialNumbers);
+
+
 function App() {
 
   return (
